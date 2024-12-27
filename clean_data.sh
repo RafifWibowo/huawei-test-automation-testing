d9308f9f2e1939168be30041cf51e9af60996100
RafifWibowo <rafifnaufaldi2@gmail.com>
2024-12-27 09:45:10 +0700

    updte init_task.sh

--- clean_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Fungsi untuk menghapus file yang lebih tua dari 30 hari
-clean_data() {
-    find ./cron -type f -mmin +2 -delete
-}
-
-# Jalankan fungsi pembersihan data
-clean_data
--- init_task.sh
+++ init_task.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-chmod +x ./collect_data.sh
-chmod +x ./clean_data.sh
+# Create /home/cron directory
+mkdir -p /home/cron
+echo "Created /home/cron directory."
 
-
-cron_jobs=(
-    "0 18,19 * * * ./collect_data.sh" 
-    "0 20 * * * ./clean_data.sh"
-)
+# Absolute paths for cron jobs
+cron_directory="/home/cron"
+resource_url="https://drive.google.com/uc?id=1zO8ekHWx9U7mrbx_0Hoxxu6od7uxJqWw&export=download"
 
 # Add cron jobs
-for cron_job in "${cron_jobs[@]}"; do
-  echo "$cron_job" | crontab -
-done
+
+# Cron job to collect resources
+(crontab -l 2>/dev/null; echo "0 8,12,15 * * * wget -O \"${cron_directory}/cron_\$(date +\%Y\%m\%d_\%H\%M).csv\" \"$resource_url\"") | crontab -
+echo "Added cron job for collecting resources."
+
+# Cron job to clean old files
+(crontab -l 2>/dev/null; echo "* * * * * find ${cron_directory} -type f -mtime +30 -exec rm {} \;") | crontab -
+echo "Added cron job for cleaning old files."
